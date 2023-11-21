89b2a33971ef68f7298e8694ad9a50eddc32afb5
Nosliejnav <vanjeilsonm10@gmail.com>
2023-11-21 15:28:56 -0300

    Validação de Login

--- js/script.js
+++ js/script.js
@@ -12,4 +12,20 @@ function topo(){
 
 //Validação de Login
 
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = "index.html";
+        logado = 1;
+    }
+
+    if (logado == 0){
+        alert("Acesso Negado. Dados incorretos");
+    }
+
+}
+
 //Ativar alert no botão cadastrar
